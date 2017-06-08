4dee3a43ee59bd9ea41ed72f5820893d05389135
hank20219 <hank20219@yahoo.com.tw>
2017-06-08 15:01:16 +0800

    Put on StartEnd trigger for automatic recording

---
+++ Racing Game-Unity/Assets/Scripts/StartEndEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEndEvent : MonoBehaviour {
+
+    void OnTriggerEnter(Collider car)
+    {
+        if(car.tag == "Player")
+        {
+            car.GetComponentInChildren<DataRecorder>().writeFile();
+            print("Data saved");
+        }
+    }
+}
